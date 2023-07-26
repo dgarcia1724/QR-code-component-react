4c4a7f93e59debf599fc36cd2f839ccd5420f147
dgarcia1724 <dgarcia1724@berkeley.edu>
2023-07-26 15:55:17 -0700

    fin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <Card />
+    </main>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <img src={require("./images/image-qr-code.png")} alt="qr code" />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
     </div>
   );
 }
